4628c7ed569febfd90af3cd4fb7028b62cd1d252
Superern <ernandrewgregorio@gmail.com>
2020-08-27 23:01:17 +0800

    add styles to message composer

--- resources/js/components/messages/MessageComposer.vue
+++ resources/js/components/messages/MessageComposer.vue
@@ -1,5 +1,8 @@
 <template>
-
+<div class="conversation-composer">
+    <label for="messageComposer">Message ...</label>
+    <textarea name="messageComposer" id="messageComposer" rows="2"></textarea>
+</div>
 </template>
 
 <script>
@@ -8,6 +11,23 @@
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.conversation-composer{
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    border-top: 1px solid #00000020;
+    padding: 15px 30px;
 
+    label{
+        color: #c3c3c3;
+    }
+    textarea{
+        resize: none;
+        border: 0;
+        &:focus{
+            outline: none;
+        }
+    }
+}
 </style>
