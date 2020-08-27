0c505ddad0f41924d09bff65c85259572e0b0f54
Superern <ernandrewgregorio@gmail.com>
2020-08-27 22:49:47 +0800

    display information of contact in conversation header

--- resources/js/components/messages/MessageHead.vue
+++ resources/js/components/messages/MessageHead.vue
@@ -1,13 +1,48 @@
 <template>
-
+<div class="conversation-header">
+    <profile-image :url="Contact.profile_image"/>
+    <div class="conversation-header-info">
+        <h3>{{ Contact.name }}</h3>
+        <span>Some Information</span>
+    </div>
+    <i class="fas fa-info-circle"></i>
+</div>
 </template>
 
 <script>
-	export default {
-		name: "MessageHead"
+    import ProfileImage from '../common/ProfileImage'
+    export default {
+		name: "MessageHead",
+        components: {ProfileImage},
+        props:{
+		    Contact:{
+		        type: Object,
+                required: true
+            }
+        }
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.conversation-header{
+    padding: 0 30px;
+    display: flex;
+    align-items: center;
+    height: 100px;
+    border-bottom: 1px solid #00000020;
 
+    &-info{
+        margin-left: 10px;
+        flex: 1;
+        h3{
+            margin: 0;
+        }
+    }
+
+    &> i {
+        font-size: 50px;
+        color: #c3c3c3;
+        cursor: pointer
+    }
+}
 </style>
