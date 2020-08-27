7e5afd97c51b62842397af65bb8525d0baeac1f0
Superern <ernandrewgregorio@gmail.com>
2020-08-27 20:29:45 +0800

    refactor scaffold components and use it in blade

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h1>Messages</h1>
+                    </div>
+
+                    <div class="card-body">
+                        <Contacts/>
+                        <Conversation/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Conversation from './Conversation';
+    import Contacts from './Contacts';
+    export default {
+        components: {
+            Conversation,
+            Contacts
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
