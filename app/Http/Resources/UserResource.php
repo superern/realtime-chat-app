d189143b6212a81cd41d1e3bfaf199248c1bb2c0
Superern <ernandrewgregorio@gmail.com>
2020-08-27 23:07:47 +0800

    add id when retrieving contacts

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'=> $this->id,
             'name'=> $this->name,
             'mobile_number'=> $this->mobile_number,
             'email'=> $this->email,
