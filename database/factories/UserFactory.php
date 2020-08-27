5d52c8b54ea44847fadab9822f1d4a653c84497e
Superern <ernandrewgregorio@gmail.com>
2020-08-27 18:58:54 +0800

    update user factory with added columns

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'mobile_number' => $faker->unique()->phoneNumber,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'profile_image' => 'https://picsum.photos/150',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
